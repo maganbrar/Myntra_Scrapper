132cee1eb9ba5c3e0346f262bca6c1274884f6c1
jattblood <choudharymagan@gmail.com>
2025-02-05 20:10:25 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="myntra_scraper",
+    version="0.0.1",
+    author="Magan Brar",
+    author_email="maganbrar@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
